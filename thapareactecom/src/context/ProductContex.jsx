f418c671e6ca955acc5629a988dd912ea604d91b
ap-lodhi <anilpatel.bcs@gmail.com>
2023-01-11 20:37:43 +0530

    reducer page  added

--- thapareactecom/src/context/ProductContex.jsx
+++ thapareactecom/src/context/ProductContex.jsx
@@ -2,23 +2,39 @@ import { useContext } from "react";
 import { createContext } from "react";
 import axios from "axios"
 import { useEffect } from "react";
+import { useReducer } from "react";
+import reducer from "../reducer/ProductReducer"
 
  const AppContext = createContext();
 
 const API = "https://api.pujakaitem.com/api/products";
+const initialState={
+    isLodaing:false,
+    isError:false,
+    products:[],
+    featureProducts:[],
+};
+
 
  const AppProvider =({children})=>{
+    const [state,dispatch] =useReducer(reducer,initialState);
     const getProducts =async(url)=>{
-        const res = await axios.get(url);
-        const products = await res.data;
-        console.log(products)
+        dispatch({ type: "SET_LOADING" });
+        try {
+            const res = await axios.get(url);
+            const products = await res.data;
+            dispatch({ type: "SET_API_DATA", payload: products });
+          } catch (error) {
+            dispatch({ type: "API_ERROR" });
+          }
 
     }
     useEffect(()=>{
+
         getProducts(API)
     },[])
         return( 
-            <AppContext.Provider value={{myName:"anil patel"}}>{children}</AppContext.Provider>
+            <AppContext.Provider value={{...state}}>{children}</AppContext.Provider>
         )
  }
 
