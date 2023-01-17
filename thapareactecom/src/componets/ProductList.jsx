515524617137d1d6915fdeab28be6231ecb76e9a
ap-lodhi <anilpatel.bcs@gmail.com>
2023-01-17 16:59:23 +0530

    error resolve grid and list view added

--- thapareactecom/src/componets/ProductList.jsx
+++ thapareactecom/src/componets/ProductList.jsx
@@ -1,21 +1,19 @@
-import React from 'react'
-import { useFilterContext } from '../context/FilterContext';
-import GridView from './GridView';
+import React from "react";
+import { useFilterContext } from "../context/FilterContext";
+
+import GridView from "./GridView";
+import ListView from "./ListView";
 
 const ProductList = () => {
-  const {filter_products ,setGridview} =useFilterContext();
-  
- 
-  // if(setGridview){
-    return(<>
-    {/* <GridView  products={filter_products}/> */}
-     {/* <h1>dvdjvbd</h1> */}
-     <div>product list</div>
-    </>
-    )
-  // }
-}
+  const { filter_products, grid_view } = useFilterContext();
 
+  if (grid_view === true) {
+    return <GridView products={filter_products} />;
+  }
 
+  if (grid_view === false) {
+    return <ListView products={filter_products} />;
+  }
+};
 
-export default ProductList ;
+export default ProductList;
--- thapareactecom/src/context/FilterContext.jsx
+++ thapareactecom/src/context/FilterContext.jsx
@@ -9,7 +9,11 @@ const FilterContext =createContext();
 const initialState={
     filter_products:[],
     all_products:[],
-    // grid_view:true
+    grid_view:true,
+    sorting_value: "lowest",
+    filters: {
+      text: "",
+    },
 }
 
 const FilterContextProvider =({children})=>{
@@ -18,15 +22,56 @@ const FilterContextProvider =({children})=>{
 
     const [state,dispatch] =useReducer(reducer, initialState)
 
-    // // to set the grid view 
-    // const setGridView=()=>{
-    //     return dispatch({type:"SET_GRID_VIEW"})
-    // }
+    // to set the grid view 
+    const setGridView=()=>{
+        return dispatch({type:"SET_GRID_VIEW"})
+    }
+
+    
+  // to set the list view
+  const setListView = () => {
+    return dispatch({ type: "SET_LIST_VIEW" });
+  };
+
+  // sorting function
+  const sorting = (event) => {
+    let userValue = event.target.value;
+    dispatch({ type: "GET_SORT_VALUE", payload: userValue });
+  };
+
+  // update the filter values
+  const updateFilterValue = (event) => {
+    let name = event.target.name;
+    let value = event.target.value;
+
+    return dispatch({ type: "UPDATE_FILTERS_VALUE", payload: { name, value } });
+  };
+
+  // to sort the product
+  useEffect(() => {
+    dispatch({ type: "FILTER_PRODUCTS" });
+    dispatch({ type: "SORTING_PRODUCTS" });
+  }, [products, state.sorting_value, state.filters]);
+
+  // to load all the products for grid and list view
+  useEffect(() => {
+    dispatch({ type: "LOAD_FILTER_PRODUCTS", payload: products });
+  }, [products]);
+
 
     useEffect(()=>{
         dispatch({type:"LOAD_FILTER_PRODUCTS",payload:products})
     },[products])
-    return <FilterContext.Provider value={{...state ,}}>
+
+
+    return <FilterContext.Provider
+     value={{...state ,
+      setGridView,
+      setListView,
+      sorting,
+      updateFilterValue,
+      
+      }}>
         {children}
     </FilterContext.Provider>
 }
