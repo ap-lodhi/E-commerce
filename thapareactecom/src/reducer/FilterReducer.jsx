4976edcdc80e1cf722e281e1d54a852d692f700d
ap-lodhi <anilpatel.bcs@gmail.com>
2023-01-26 12:55:08 +0530

    inputrange field done

--- thapareactecom/src/reducer/FilterReducer.jsx
+++ thapareactecom/src/reducer/FilterReducer.jsx
@@ -76,7 +76,7 @@ const filterReducer = (state, action) => {
         let { all_products } = state;
         let tempFilterProduct = [...all_products];
   
-        const { text, category, company, color } = state.filters;
+        const { text, category, company, color,price } = state.filters;
   
         if (text) {
           tempFilterProduct = tempFilterProduct.filter((curElem) => {
@@ -99,6 +99,15 @@ const filterReducer = (state, action) => {
         if (color !== "all") {
           tempFilterProduct = tempFilterProduct.filter((curElem) =>
             curElem.colors.includes(color)
+          );
+        }
+        if(price ===0){
+          tempFilterProduct =tempFilterProduct.filter(
+            (curElem)=> curElem.price<=price
+          );
+        }else{
+          tempFilterProduct =tempFilterProduct.filter(
+            (curElem)=> curElem.price<=price
           );
         }
         return {
