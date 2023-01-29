032f2893a27a49d413725b9bb547ce05d0e516c8
ap-lodhi <anilpatel.bcs@gmail.com>
2023-01-29 16:49:50 +0530

    increment decrememt  done

--- thapareactecom/src/context/CartContext.jsx
+++ thapareactecom/src/context/CartContext.jsx
@@ -28,6 +28,18 @@ const CartProvider = ({ children }) => {
     dispatch({ type: "ADD_TO_CART", payload: { id, color, amount, product } });
   };
 
+
+
+  // increment and decrement the product
+
+  const setDecrease = (id) => {
+    dispatch({ type: "SET_DECREMENT", payload: id });
+  };
+
+  const setIncrement = (id) => {
+    dispatch({ type: "SET_INCREMENT", payload: id });
+  };
+
   const removeItem = (id) => {
     dispatch({ type: "REMOVE_ITEM", payload: id });
   };
@@ -39,15 +51,19 @@ const CartProvider = ({ children }) => {
     dispatch({type:"CLEAR_CART"});
   }
 // to add data in to localstorage
+useEffect(() => {
+  // dispatch({ type: "CART_TOTAL_ITEM" });
+  // dispatch({ type: "CART_TOTAL_PRICE" });
+  dispatch({ type: "CART_ITEM_PRICE_TOTAL" });
 
-useEffect(()=>{
-  localStorage.setItem("thapaCart", JSON.stringify(state.cart))
-
-},[state.cart])
+  localStorage.setItem("thapaCart", JSON.stringify(state.cart));
+}, [state.cart]);
 
 
   return (
-    <CartContext.Provider value={{ ...state, addToCart, removeItem ,clearCart}}>
+    <CartContext.Provider
+     value={{ ...state, addToCart, removeItem ,clearCart,setDecrease,
+      setIncrement,}}>
       {children}
     </CartContext.Provider>
   );
